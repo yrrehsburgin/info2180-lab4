1972ec6301756912055d685892421951ed043049
Sherricea Burgin <yrrehsburgin@outlook.com>
2018-10-23 12:46:41 -0500

    Use inner HTML

--- maze.js
+++ maze.js
@@ -5,22 +5,34 @@ boundary1 = document.getElementById("boundary1")
 var x = document.querySelectorAll(".boundary");
 var boundary = document.getElementsByClassName("boundary");
 var end = document.getElementById("end");
+var start = document.getElementById("start");
+var status = document.getElementById("status");
 var a;
 var i;
 
 for(a=0; a< boundary.length; a++){
     x[a].onmouseover = function(){
         for(i=0; i< boundary.length; i++){
+            /*alert("YOU LOSE!!");*/
+            status.innerHTML = "YOU LOSE!!";
             x[i].className += " youlose"; 
         }
     }
 }
 
 end.onmouseover = function(){
-    alert("YOU WIN!!!");
+    //alert("YOU WIN!!!");
+    status.innerHTML = "YOU WIN!!!"
+
 }
+
+start.onclick = function(){
+    location.reload();
+}
+
 boundary1.onmouseover = function(){
     boundary1.className += " youlose";
+    status.innerHTML = "YOU LOSE!!";
 }
 
 
