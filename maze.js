c2b5ac9adc00ef98a2a5baa07815b7f3c84575e4
Sherricea Burgin <yrrehsburgin@outlook.com>
2018-10-23 11:08:27 -0500

    On mouseover all boundaries turn red

--- maze.js
+++ maze.js
@@ -2,6 +2,19 @@ window.onload = function(){
 alert("gggg");
 boundary1 = document.getElementById("boundary1")
 
+var x = document.querySelectorAll(".boundary");
+var boundary = document.getElementsByClassName("boundary");
+var a;
+var i;
+
+for(a=0; a< boundary.length; a++){
+    x[a].onmouseover = function(){
+        for(i=0; i< boundary.length; i++){
+            x[i].className += " youlose"; 
+        }
+    }
+}
 boundary1.onmouseover = function(){
     boundary1.className += " youlose";
+}
 }
